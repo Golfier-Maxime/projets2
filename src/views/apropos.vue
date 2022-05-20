84d023ee4ee774327d6570a74c6dfdfa189665e2
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2022-05-20 09:24:09 +0200

    creation apropro

--- src/views/apropos.vue
+++ src/views/apropos.vue
@@ -1,13 +1,47 @@
 <template>
-  <p>deededd</p>
-  <p>deededd</p>
-  <p>deededd</p>
-  <p>deededd</p>
-  <p>deededd</p>
-  <p>deededd</p>
-  <p>deededd</p>
+  <div class="mb-3 pt-[45px] text-center text-3xl text-white">
+      <h2 class="font-prompt">À propos</h2>
+    </div>
+  <div class="bg-white rounded-2xl">
+    <div class=" ml-6 mr-6">
+      <h3 class="mb-2 font-medium text-2xl text-center ml-6 mr-6 font-prompt">Tout savoir sur le projet Axolott</h3>
+      <p class="font-mulish mb-2">Axolott et un projet réalisé dans le cadre d’un exercice pédagogique au département MMi de Montbéliard
+      </p>
+      <img src="public\icon\logo-mmi-noir-blanc.svg" alt="" class="mx-auto mb-2">
+      <p>Axolott et une façon de modifier ou améliorer votre routine. Le but et de vous accompagnez dans votre vie de tout les jours afin de dynamiser votre quotidien.
+      </p>
+      <h3 class="mb-2 font-medium text-2xl text-center ml-6 mr-6 font-prompt">Rencontrez les créateurs d’Axolott</h3>
+      <div>
+        <h3 class="mb-2 font-medium text-2xl text-center  font-prompt">Maxime GOLFIER</h3>
+        <div>  
+          <div class="mb-3 text-center text-3xl text-white">
+            <h2 class="font-prompt">Pseudo</h2>
+          </div>
+          <div class="flex justify-center">
+            <div class="cercle_bleu">
+              <img src="public/axolott/axo_main.svg" alt="axolotl de base" class="img_axo" />
+            </div>
+          </div>
+        </div>
+        <p>Étudiant en première année de MMI, je suis passionné par la programmation et le dévelopement web.
+           Vous pouvez me retrouver sur mes réseaux !</p>
+      </div>
+    </div>
+  </div>
 </template>
 <style>
+.cercle_bleu {
+  width: 300px;
+  height: 300px;
+  border-radius: 100%;
+  background: var(--cyan);
+}
+:root {
+  --cyan: #08a2f1;
+}
+.img_axo {
+  margin-top: 50px;
+}
 </style>
 
 <script>
