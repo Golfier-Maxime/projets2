035317c0af4f78028c05fb3e213da288c02129ee
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2022-06-08 12:59:20 +0200

    import firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,24 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAUmKeWJQVuB7pCLH51QihJOfpzxPXXr2I",
+    authDomain: "projet-s2-fa4a9.firebaseapp.com",
+    projectId: "projet-s2-fa4a9",
+    storageBucket: "projet-s2-fa4a9.appspot.com",
+    messagingSenderId: "409826824151",
+    appId: "1:409826824151:web:410e6329ec32c124754592"
+};
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+
+
 const app = createApp(App)
 
 app.use(router)
